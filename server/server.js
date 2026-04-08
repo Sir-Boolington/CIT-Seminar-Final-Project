d926ae300d73a6619f23f664ff8dba33063e4306
jayvals <jeromevallido@gmail.com>
2026-04-08 12:30:21 -0400

    Update server.js
    Added sessionRoutes to beginning and "Routes" section. Session info should now be saved/updated.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const pool = require('./config/db');
+const sessionRoutes = require('./routes/sessions');
 
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.json());
 
 // ——— Routes ———
 app.use('/api/auth', authRoutes);
+app.use('/api/sessions', sessionRoutes);
 
 // Health check
 app.get('/api/health', (req, res) => {
